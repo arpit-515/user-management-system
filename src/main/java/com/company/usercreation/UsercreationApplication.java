5737581bc0ce65e5952f1165fd8fd35412c89221
arpit-515 <arpitsingh.05150@gmail.com>
2026-02-05 16:43:24 +0530

    ADDED WAR DEPENDENCY

--- src/main/java/com/company/usercreation/UsercreationApplication.java
+++ src/main/java/com/company/usercreation/UsercreationApplication.java
@@ -2,14 +2,20 @@ package com.company.usercreation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
 @SpringBootApplication
-public class UsercreationApplication {
+public class UsercreationApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(UsercreationApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(UsercreationApplication.class, args);
     }
-
 }
